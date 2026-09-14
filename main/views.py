c662a43465d4342cea81eda7edf53558103c44cf
bobbybhskara <m.eshan.bobby@gmail.com>
2026-09-14 13:34:37 +0700

    feat: add dynamic projects page

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Project
 
 
 def show_main(request):
@@ -24,3 +24,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_projects(request):
+    context = {
+        "name": "Muhammad Eshan Bobby Bhaskara",
+        "project_list": Project.objects.order_by("display_order", "title"),
+    }
+    return render(request, "projects.html", context)
